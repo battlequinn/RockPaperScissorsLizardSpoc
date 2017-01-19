24d5680af5c32ad64da31b30e93048f3cb2efc15
battlequinn <quinnthebattle@gmail.com>
2017-01-19 09:52:04 -0600

    Corrected spoc to spock lol. Initial commit. created class RockPaperScissorsLizzardSpock, Multiplayer, and SinglePlayer. created a welcome, a main menu with choices to play singleplayer, and got multiplayer input for both players.

---
+++ RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsLizardSpock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RockPaperScissorsLizardSpock Spock = new RockPaperScissorsLizardSpock();
+            Multiplayer multiplayer = new Multiplayer();
+            Singleplayer singleplayer = new Singleplayer();
+            Spock.DisplayWelcome();
+            int mode = Spock.ChooseGameMode();
+            if(mode == 1){
+                singleplayer.RunSingleplayer();
+            }
+            else
+            {
+                multiplayer.RunMultiplayer();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsLizardSpock
+{
+    class RockPaperScissorsLizardSpock
+    {
+        public void DisplayWelcome()
+        {
+            Console.WriteLine("=======================================================================================");
+            Console.WriteLine("Welcome to Rock Paper Scisors Lizard Spock!");
+            Console.WriteLine("=======================================================================================");
+            Console.WriteLine("The rules are simple:\n");
+            Console.WriteLine("Scissors cuts Paper \nPaper covers Rock");
+            Console.WriteLine("Rock crushes Lizard \nLizard poisons Spock");
+            Console.WriteLine("Spock smashes Scissors \nScissors decapitates Lizard");
+            Console.WriteLine("Lizard eats Paper \nPaper disproves Spock");
+            Console.WriteLine("Spock vaporizes Rock \nand as it always has");
+            Console.WriteLine("Rock crushes Scissors");
+            Console.WriteLine("=======================================================================================");
+        }
+        public int ChooseGameMode()
+        {
+            Console.WriteLine("\nMain Menu");
+            Console.WriteLine("___________");
+            Console.WriteLine("\nPlease enter the number corresponding to the game mode you would like to play.");
+            bool reset = false;
+            int answer;
+            do
+            {
+                Console.WriteLine("\n1) Singleplayer\n2) Multiplayer");
+                bool result = Int32.TryParse(Console.ReadLine(), out answer);
+                if (result && (answer == 1 || answer == 2))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Unable to read input. Please type the number '1' or '2'.");
+                    reset = true;
+                }
+            } while (reset == true);
+            return answer;
+        }
+
+
+    }
+}
